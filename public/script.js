fc1f0e5bea0891f135c0cf9d3a85cb784e0da5c3
Alumno11 J601 <a11j601@campustls.local>
2017-11-28 12:58:36 -0500

    test

--- public/script.js
+++ public/script.js
@@ -1,31 +1,20 @@
 $(document).ready(function () {
 	$('#btn').on("click", function(e) {
 		e.preventDefault();
-		var nombre = $('#nombre').val();
-		var sexo = $('input[name="gender"]:checked').val();
-		var mensaje = $('#textA').val();
-		var hobbie = $('input[name="hobbies"]:checked');
-		var hobbies = new Array();
-
-		for (var i = 0; i < hobbie.length; i++) {
-			var temp = hobbie[i];
-			hobbies.push(temp.value);
-		}
+		var nombre = $('#first_name').val();
+		var apellido = $('#last_name').val();
 		var params = {
-			'nombre':nombre,
-			'sexo': sexo,
-			'mensaje': mensaje,
-			'hobbies': hobbies
+			'nombre': nombre,
+			'apellido': apellido
 		}
-
-		$.get('/find', params)
-			.done(function(data) {
-				console.log(data.data);
-			});
 
 		$.post('/find', params)
 			.done(function(data) {
 				console.log(data.data);
 			});
+
+		var message = document.querySelectorAll("#message")[0];
+		message.innerHTML = message.innerHTML + "<span>" + nombre + "</span>";
+
 	});
 });
--- public/vanillajs-eventos/main.js
+++ public/vanillajs-eventos/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+/*
 	console.log("listo!");
 
 	var forms = document.querySelectorAll("form"); // esto es igual a $('form') / Busqueda
@@ -6,9 +7,39 @@ document.addEventListener("DOMContentLoaded", function() {
 	
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
-		var nombre = document.querySelectorAll("#nombre");
-		console.log(nombre);
+		var nombre = document.querySelectorAll("#nombre")[0].value;
+		
+		var message = document.querySelectorAll("#message")[0];
+		message.innerHTML = message.innerHTML + "<span>" + nombre + "</span>";
 	});
 
-	console.log(form);
+	console.log(form); */
+
+	/*var elements = document.querySelectorAll("li");
+
+	for (var i = 0; i < elements.length; i = i + 1) {
+		var element = elements[i];
+
+		element.addEventListener("click", function(event) {
+			var el = event.target;
+
+			console.log(el.innerHTML);
+		});
+	};*/
+
+	var lista = document.querySelectorAll("ul")[0];
+	
+	lista.addEventListener("click", function(event) {
+		var el = event.target;
+
+		/*
+		if (el.tagName.toLowerCase() === "li") {
+			console.log("Click en:" , el.innerHTML);
+		}
+		*/
+
+		if (el.classList.contains("item")) {
+			console.log("Click en:" , el.innerHTML);//innerHTML devuelve el contenido html de un elemento
+		} //classList es metodo que devuelve la lista de clases de un elemento
+	});
 });
